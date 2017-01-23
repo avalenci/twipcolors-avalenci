bea29a2566d49b8d0b88d164a2e3174454d8cd7b
avalenci <ansmeew@gmail.com>
2017-01-23 14:24:52 -0500

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("I will convert your colors! Which format will you use?\n" +
+                "(A) Hexadecimal\n(B) RGB\n(C) HTML";
+        boolean done = true;
+        while (done) {
+            String ans = input.nextLine();
+            if (ans.equalsIgnoreCase("A")) {
+                done = false;
+            }
+            else if (ans.equalsIgnoreCase("B")) {
+                done = false;
+            }
+            else if (ans.equalsIgnoreCase("C")) {
+                done = false;
+            }
+            else
+                System.out.println("Try again.");
+        }
+
+    }
+}
