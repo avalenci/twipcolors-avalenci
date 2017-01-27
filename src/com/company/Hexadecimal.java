a760f1e5e2641e6452a027a383040c72de02810c
avalenci <ansmeew@gmail.com>
2017-01-26 23:15:46 -0500

    Fourth commit.

--- src/com/company/Hexadecimal.java
+++ src/com/company/Hexadecimal.java
@@ -1,4 +1,34 @@
 package com.company;
 
+import java.util.Scanner;
 public class Hexadecimal {
+    Hexadecimal(String [] c) {
+        RGB = toRGB(c);
+        HTML = toHTML(c);
+    }
+    int[] toRGB(String[] a) {
+        int count = 0;
+        int x[] = {1};
+        return x;
+    }
+    String toHTML(String[] b) {
+        return "Blue";
+    }
+    public static boolean checker(String h) {
+        if (h.length() != 6)
+            return false;
+        Scanner input = new Scanner(h);
+        int count = 0;
+        input.useDelimiter("");
+        while (count < h.length()) {
+            char a = input.next().charAt(0);
+            int b = (int)a;
+            if (!(b > 47 && b < 58 || b > 64 && b < 71 || b > 96 && b < 103))
+                return false;
+            count++;
+        }
+        return true;
+    }
+    public int []RGB = new int[3];
+    public String HTML;
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,23 @@ public class Main {
             String ans = input.nextLine();
             if (ans.equalsIgnoreCase("A")) {
                 done = false;
+                System.out.println("Please type in your hexadecimal.");
+                String orig;
+                while (true) {
+                    orig = input.nextLine();
+                    if (Hexadecimal.checker(orig))
+                        break;
+                    else if (orig.contains(" "))
+                        System.out.println("Please do not use space.");
+                    System.out.println("Try again.");
+                }
+                String []hex = new String[3];
+                hex[0] = orig.substring(0, 2);
+                hex[1] = orig.substring(2, 4);
+                hex[2] = orig.substring(4);
+                Hexadecimal dec = new Hexadecimal(hex);
+                System.out.println("RGB: " + dec.RGB[0] + ", " + dec.RGB[1] + ", " +
+                        dec.RGB[2] + "\nHTML: " + dec.HTML);
             }
             else if (ans.equalsIgnoreCase("B")) {
                 done = false;
